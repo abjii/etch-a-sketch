08aa86847b0666cf27269857e1d9c18462371859
abjii <156369372+abjii@users.noreply.github.com>
2025-11-06 21:13:17 +0100

    make button for choosing number boxes in grid

--- eas_script.js
+++ eas_script.js
@@ -1,23 +1,33 @@
-const perRowBoxes = 10;
+let perRowBoxes = 16;
+const nRowsButton = document.getElementById("nRowsButton");
 const container = document.getElementById("container");
-const boxSize = 100 / perRowBoxes; //percentage of box width and height
 
-function makeGrid() {
+function makeGrid(perRowBoxes) {
+  const boxSize = 100 / perRowBoxes; //percentage of box width and height
   for (let i = 0; i < perRowBoxes; i++) {
     for (let j = 0; j < perRowBoxes; j++) {
       const div = document.createElement("div");
       div.style.width = boxSize + "%";
       div.style.height = boxSize + "%";
       container.appendChild(div);
-
       div.addEventListener("mouseover", changeColor);
-
-      function changeColor(event) {
-        event.target.style.backgroundColor = "black";
-        console.log(event);
-      }
     }
   }
 }
+function changeColor(event) {
+  event.target.style.backgroundColor = "black";
+}
 
-makeGrid();
+makeGrid(perRowBoxes);
+
+nRowsButton.addEventListener("click", () => {
+  let x = parseInt(prompt("how many boxes should be in the grid (choose a number under 100)"));
+  console.log("typeof x " + typeof x);
+  console.log(" x =  " + x);
+  if (typeof x === "number" && x < 100) {
+    container.innerHTML = ""; // to improve for security reasons maybe !!
+    makeGrid(x);
+  } else {
+    console.log(x + "not nmber");
+  }
+});
