530c3a8ed98861eadd2a4422192e3c09c3676b79
abjii <156369372+abjii@users.noreply.github.com>
2025-11-11 19:26:02 +0100

    add Change Grid Size button

--- eas_script.js
+++ eas_script.js
@@ -1,9 +1,13 @@
 let perRowBoxes = 16;
 const nRowsButton = document.getElementById("nRowsButton");
 const container = document.getElementById("container");
+const h2 = document.getElementsByTagName("h2").item(0);
 
 function makeGrid(perRowBoxes) {
-  const boxSize = 100 / perRowBoxes; //percentage of box width and height
+  container.innerHTML = "";
+  if (between(perRowBoxes, 0, 100))
+    h2.textContent = "Change number of pixels in grid (" + perRowBoxes + " x " + perRowBoxes + ")";
+  const boxSize = 100 / perRowBoxes; //get percentage of box width and height
   for (let i = 0; i < perRowBoxes; i++) {
     for (let j = 0; j < perRowBoxes; j++) {
       const div = document.createElement("div");
@@ -14,20 +18,28 @@ function makeGrid(perRowBoxes) {
     }
   }
 }
+
 function changeColor(event) {
   event.target.style.backgroundColor = "black";
 }
 
-makeGrid(perRowBoxes);
+nRowsButton.addEventListener("click", setNewGrid);
 
-nRowsButton.addEventListener("click", () => {
-  let x = parseInt(prompt("how many boxes should be in the grid (choose a number under 100)"));
-  console.log("typeof x " + typeof x);
-  console.log(" x =  " + x);
-  if (typeof x === "number" && x < 100) {
-    container.innerHTML = ""; // to improve for security reasons maybe !!
-    makeGrid(x);
+function setNewGrid(event) {
+  perRowBoxes = parseInt(prompt("how many boxes should be in the grid (choose a number under 100)"));
+  console.log("typeof x = " + typeof perRowBoxes);
+  console.log("value x = " + perRowBoxes);
+  if (typeof perRowBoxes === "number" && between(perRowBoxes, 0, 100)) {
+    container.innerHTML = "";
+    makeGrid(perRowBoxes);
   } else {
-    console.log(x + "not nmber");
+    alert("please enter a number between 1 and 99");
+    makeGrid(0);
   }
-});
+}
+
+function between(x, min, max) {
+  return x > min && x < max;
+}
+
+makeGrid(perRowBoxes);
