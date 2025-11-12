ab4368e6d633bc966911712e4ce81d614c2b6625
abjii <156369372+abjii@users.noreply.github.com>
2025-11-12 18:25:13 +0100

    change color when clicking and while hovering on cells only

--- eas_script.js
+++ eas_script.js
@@ -1,39 +1,58 @@
+// ====== GLOBAL VARIABLES ======
 let perRowBoxes = 16;
+let mouseDown = false;
 const nRowsButton = document.getElementById("nRowsButton");
 const container = document.getElementById("container");
 const h2 = document.getElementsByTagName("h2").item(0);
+const h3 = document.getElementsByTagName("h3").item(0);
 
+// ====== EVENT DECLARATION ======
+nRowsButton.addEventListener("click", setNewGrid);
+document.addEventListener("mousedown", () => {
+  mouseDown = true;
+});
+document.addEventListener("mouseup", () => {
+  mouseDown = false;
+  console.log("mouseDown = false;");
+});
+
+// ====== FUNCTION DECLARATION ======
 function makeGrid(perRowBoxes) {
   container.innerHTML = "";
-  if (between(perRowBoxes, 0, 100))
-    h2.textContent = "Change number of pixels in grid (" + perRowBoxes + " x " + perRowBoxes + ")";
-  const boxSize = 100 / perRowBoxes; //get percentage of box width and height
+  if (between(perRowBoxes, 0, 100)) {
+    h2.textContent = "Change number of pixels in grid";
+    h3.textContent = `(${perRowBoxes} x ${perRowBoxes})`;
+  }
+  const boxSize = 100 / perRowBoxes; // percentage width/height of each box
   for (let i = 0; i < perRowBoxes; i++) {
     for (let j = 0; j < perRowBoxes; j++) {
       const div = document.createElement("div");
       div.style.width = boxSize + "%";
       div.style.height = boxSize + "%";
+      div.addEventListener("mousedown", changeColor);
+      div.addEventListener("mouseover", hoverColor);
       container.appendChild(div);
-      div.addEventListener("mouseover", changeColor);
     }
   }
 }
 
 function changeColor(event) {
   event.target.style.backgroundColor = "black";
+  console.log("mouse down so colouring");
 }
 
-nRowsButton.addEventListener("click", setNewGrid);
+function hoverColor(event) {
+  if (mouseDown) {
+    event.target.style.backgroundColor = "black";
+  }
+}
 
-function setNewGrid(event) {
-  perRowBoxes = parseInt(prompt("how many boxes should be in the grid (choose a number under 100)"));
-  console.log("typeof x = " + typeof perRowBoxes);
-  console.log("value x = " + perRowBoxes);
+function setNewGrid() {
+  perRowBoxes = parseInt(prompt("How many boxes should be in the grid (choose a number under 100)"));
   if (typeof perRowBoxes === "number" && between(perRowBoxes, 0, 100)) {
-    container.innerHTML = "";
     makeGrid(perRowBoxes);
   } else {
-    alert("please enter a number between 1 and 99");
+    alert("Please enter a number between 1 and 99");
     makeGrid(0);
   }
 }
@@ -42,4 +61,5 @@ function between(x, min, max) {
   return x > min && x < max;
 }
 
+// ====== CALLS ======
 makeGrid(perRowBoxes);
